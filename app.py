31869af6fbbcf1e9fb4a87ba7948a202ebc4958a
Sanket Saurav <sanketsaurav@gmail.com>
2014-05-13 22:32:20 +0530

    Adding a few pages.

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 	return render_template('home.html')
 
-@app.route('/team')
-def team():
-	return render_template('team.html')
+@app.route('/people')
+def people():
+	members = json.load(open('members.json', 'r'))
+	return render_template('team.html', members = members['data'])
+
+@app.route('/aircrafts')
+def aircrafts():
+	return render_template('aircrafts.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
